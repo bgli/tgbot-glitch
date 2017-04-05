57602d6ff1f73914f2513c91389a80cd171e84b0
Sucipto <show.cheap@gmail.com>
2017-04-05 17:25:24 +0700

    Update package

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const Telegraf = require('telegraf')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const appName = process.env.PROJECT_NAME
+const appPort = process.env.PORT
 
 // Set Webhook
-bot.telegram.setWebhook('https://domainmu.glitch.me/webhook')
+bot.telegram.setWebhook(`https://${appName}.glitch.me/webhook`)
 
 
 // Start builtin Webhook
-bot.startWebhook('/webhook', null, 3000)
+bot.startWebhook('/webhook', null, appPort)
 
 // Listener
-bot.on('text', (ctx) => ctx.reply('Hello Human')
+bot.on('text', (ctx) => ctx.reply('Hello Human'))
